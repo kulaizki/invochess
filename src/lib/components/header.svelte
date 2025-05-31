a46fdc09797f074e957ac5c2e1dacf24947a35e9
kulaizki <kulaizke@gmail.com>
2025-06-01 00:07:01 +0800

    style: update header navigation links to replace 'Features' with 'Login' and adjust 'Sign Up' to 'Register' for improved clarity

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -4,8 +4,8 @@
       Invochess
     </a>
     <div>
-      <a href="#key-features" class="px-3 py-2 font-semibold rounded-md hover:bg-sky-500/10 transition-colors duration-150">Features</a>
-      <a href="/signup" class="px-3 py-2 font-semibold rounded-md hover:bg-sky-500/10 transition-colors duration-150">Sign Up</a>
+      <a href="/login" class="px-3 py-2 font-semibold rounded-md hover:bg-sky-500/10 transition-colors duration-150">Login</a>
+      <a href="/signup" class="px-3 py-2 font-semibold rounded-md hover:bg-sky-500/10 transition-colors duration-150">Register</a>
       </div>
   </div>
 </nav> 
