e3e2002a4dd06b983c6404acfbab125ee84caf50
kulaizki <kulaizke@gmail.com>
2025-06-01 00:39:00 +0800

    style: change main background gradient direction for improved visual appeal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex flex-col min-h-screen">
 	<Header/>
-	<main class="flex-grow flex flex-col pt-16 bg-gradient-to-b from-gray-900 to-gray-950">
+	<main class="flex-grow flex flex-col pt-16 bg-gradient-to-br from-gray-900 to-gray-950">
 		<slot />
 	</main>
 	<Header/>
