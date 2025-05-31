fd34d93e3ca5599b0afdf4f9df5049c1ac48b876
kulaizki <kulaizke@gmail.com>
2025-05-31 21:32:20 +0800

    refactor: remove Navbar component and replace it with Header component in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import Footer from '$lib/components/footer.svelte';
-	import Navbar from '$lib/components/navbar.svelte';
+	import Header from '$lib/components/header.svelte';
 </script>
 
 <div class="flex flex-col min-h-screen">
-	<Navbar />
+	<Header/>
 	<main class="flex-grow flex flex-col pt-16 bg-gradient-to-b from-gray-900 to-gray-950">
 		<slot />
 	</main>
-	<Footer />
+	<Header/>
 </div>
