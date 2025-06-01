0146861a8cf929f651c820f3716f3c2150322d78
kulaizki <kulaizke@gmail.com>
2025-06-01 16:35:48 +0800

    style: update footer link to improve security by modifying rel attribute

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -44,7 +44,7 @@
         <a
           href="https://fitzsixto.com"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
           class="[text-shadow:0_0_8px_rgba(0,255,8,0.4)] bg-gradient-to-r from-sky-300 to-sky-600 hover:from-sky-200 hover:to-sky-500 bg-clip-text text-transparent font-bold transition-all ease-in-out duration-500 delay-300"
         >
           Fitzsixto 
