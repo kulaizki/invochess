62176f04d9f16425d926e5f3df39b71a3f99f7b9
kulaizki <kulaizke@gmail.com>
2025-06-04 13:35:26 +0800

    feat: implement conditional rendering and transition effects for user session in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 	import { onMount } from 'svelte';
 	import { supabase } from '$lib/supabaseClient';
 	import { user } from '$lib/store';
+	import { page } from '$app/stores';
+	import { fly } from 'svelte/transition';
 
 	onMount(() => {
 		// Check initial session state
@@ -27,7 +29,17 @@
 <div class="flex flex-col min-h-screen">
 	<Header/>
 	<main class="flex-grow flex flex-col pt-16 bg-gradient-to-br from-gray-900 to-gray-950">
-		<slot />
+		{#if $user}
+			{#key $page.url.pathname}
+				<div class="flex-grow flex flex-col" in:fly={{ y: 20, duration: 500, delay: 200 }}>
+					<slot />
+				</div>
+			{/key}
+		{:else}
+			<div class="flex-grow flex flex-col">
+				<slot />
+			</div>
+		{/if}
 	</main>
 	<Footer/>
 </div>
